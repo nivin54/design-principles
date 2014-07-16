77ad340db5f2d65a79787dd9399781c1791a323d
aln <nivinmail05@gmail.com>
2014-07-16 21:06:14 +0530

    First Commit from IntelliJ IDEA

---
+++ src/src/JavaConcepts.java
@@ -0,0 +1,10 @@
+/**
+ * Created by aln on 16/07/14.
+ */
+public class JavaConcepts {
+
+    public static void main(String[] args) {
+        System.out.println("Main Method, invokes Other Classes from here.");
+    }
+
+}
